bd0d49fcc09d2a87a96031f0efcf426a2152786a
[Awaxx] <boxx1@free.fr>
2021-03-04 21:17:22 +0100

    fix ctrl+n (open newfile), which doesn't provide a filename.

--- data/core/init.lua
+++ data/core/init.lua
@@ -229,11 +229,13 @@ end
 
 function core.open_doc(filename)
   -- try to find existing doc for filename
-  local abs_filename = system.absolute_path(filename)
-  for _, doc in ipairs(core.docs) do
-    if doc.filename
-    and abs_filename == system.absolute_path(doc.filename) then
-      return doc
+  if filename then
+    local abs_filename = system.absolute_path(filename)
+    for _, doc in ipairs(core.docs) do
+      if doc.filename
+      and abs_filename == system.absolute_path(doc.filename) then
+        return doc
+      end
     end
   end
   local doc = Doc(filename)
