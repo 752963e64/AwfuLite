df4a21e86f0256d61b320e401c293bfe710a4aa2
[Awaxx] <boxx1@free.fr>
2021-05-24 22:39:38 +0200

    avoid processing shit when not visible or even when gaining a false focus...
    You may assume a false focus is when you enter an area with a pending state from previous area you coming...
    oops in da code :Ð

--- data/plugins/treeview.lua
+++ data/plugins/treeview.lua
@@ -169,7 +169,7 @@ end
 
 
 function TreeView:on_mouse_pressed(button, x, y, clicks)
-  if not self.visible and x > self.width then return end
+  if not self.visible or x > self.width then return end
   if button == "left" then
     self._update = true
     if not self.hovered_item then
