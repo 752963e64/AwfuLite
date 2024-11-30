bc7724e29c14ebd3a72b181c1045c4564f8ad26e
DragonTalk <752963e64@tutanota.com>
2024-11-30 05:12:51 +0100

    never bumped the version so the jump is understandable... It's far the original build

--- src/main.c
+++ src/main.c
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
   }
   lua_setglobal(L, "ARGS");
 
-  lua_pushstring(L, "1.06");
+  lua_pushstring(L, "2.0b");
   lua_setglobal(L, "VERSION");
 
   lua_pushstring(L, SDL_GetPlatform());
